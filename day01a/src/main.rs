be342e405d5214ae6eaa7cf866a7cbfcf78be871
Huan <w3huannguyen@gmail.com>
2022-12-06 18:51:02 -0700

    Update gitignore, old day 1 changes

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,33 +1,29 @@
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
-    let contents: String = fs::read_to_string("input.txt").expect("Error in reading file");
-    let split = contents.split_whitespace();
+    let contents: String = fs::read_to_string("./src/input.txt").expect("Error in reading file");
+    let split: Vec<&str> = contents.split("\n").collect();
+    // println!("{:?}", split);
+    let mut max_cals = 0;
+    let mut curr_cals = 0;
 
-    let mut v: Vec<i32> = Vec::new();
     for s in split {
+        if s == "" {
+            curr_cals = 0;
+            continue;
+        }
         let num: i32 = match s.trim().parse() {
             Ok(n) => n,
             Err(_) => {
                 println!("Error parsing string {s}");
-                9999 // TODO improve error handling
+                -1 // TODO improve error handling
             },
         };
-        v.push(num);
-    }
-
-    let l = v.len();
-    let mut i = 0;
-    let mut count = 0;
-    while i < l -3 {
-        let a = v[i] + v[i+1] + v[i+2];
-        let b = v[i+1] + v[i+2] + v[i+3];
-        if a < b {
-            count += 1;
+        curr_cals += num;
+        if curr_cals > max_cals {
+            max_cals = curr_cals;
         }
-        i += 1;
     }
 
-    println!("Count: {count}");
+    println!("Max calories: {max_cals}");
 }
